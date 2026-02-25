60800e85439fe6c6ffb7ffb85a44088713fd4a51
andhykakurniawan <74031708+andhykakurniawan@users.noreply.github.com>
2026-02-25 23:33:45 +0700

    add postgre connection

---
+++ src/main/java/com/example/digital/payment/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.digital.payment;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend Digital Payment Alive 🚀";
+    }
+}
